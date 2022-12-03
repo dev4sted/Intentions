39aa8cb9028ec13e0db675ac37a17dd88611e8c4
Alexandr Dobrin <dobrin.alexandrr@gmail.com>
2022-12-03 21:55:45 +0300

    Add demo main menu.

--- Intentions.Pl/Menus/MainMenu.cs
+++ Intentions.Pl/Menus/MainMenu.cs
@@ -1,16 +1,47 @@
 ﻿using Intentions.Pl.Menus.Abstract;
+using Spectre.Console;
 
 namespace Intentions.Pl.Menus;
 
 internal sealed class MainMenu : DialogMenu<string>
 {
-    protected override string Ask()
+    public static class Options
     {
-        throw new NotImplementedException();
+        public const string AddIntention = "Новое намерение";
+
+        public const string SelectIntention = "Выбрать намерение";
+
+        public const string AddIntentionsGroup = "Добавить группу";
+
+        public const string SelectIntentionsGroup = "Список групп";
+
+        public const string Quit = "Выйти";
     }
 
-    protected override void ProcessAnswer(string answer)
+    protected override string Ask()
     {
-        throw new NotImplementedException();
+        SelectionPrompt<string> prompt = new SelectionPrompt<string>()
+            .AddChoices(
+                Options.AddIntention,
+                Options.SelectIntention,
+                Options.AddIntentionsGroup,
+                Options.SelectIntentionsGroup,
+                Options.Quit)
+            .UseConverter(
+                option => option.Equals(Options.Quit, StringComparison.OrdinalIgnoreCase) ?
+                    (option + ".") :
+                    (option + ";"));
+
+        return AnsiConsole.Prompt(prompt);
     }
+
+    protected override bool ProcessAnswer(string answer) => answer switch
+    {
+        Options.AddIntention => false,
+        Options.SelectIntention => false,
+        Options.AddIntentionsGroup => false,
+        Options.SelectIntentionsGroup => false,
+        _ => false
+    };
 }
+
