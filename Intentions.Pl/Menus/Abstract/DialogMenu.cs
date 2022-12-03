420ada912e348659a0f68ee2e94b9e60db8c6875
Alexandr Dobrin <dobrin.alexandrr@gmail.com>
2022-12-03 21:37:06 +0300

    Define dialog menus model.

---
+++ Intentions.Pl/Menus/Abstract/DialogMenu.cs
@@ -0,0 +1,18 @@
+﻿namespace Intentions.Pl.Menus.Abstract;
+
+internal abstract class DialogMenu<TAnswer>
+{
+    private bool isClosed = false;
+
+    public void Start()
+    {
+        while (!isClosed)
+            ProcessAnswer(Ask());
+    }
+
+    protected abstract TAnswer Ask();
+
+    protected abstract void ProcessAnswer(TAnswer answer);
+
+    public void Stop() => isClosed = true;
+}
---
+++ Intentions.Pl/Menus/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using Intentions.Pl.Menus.Abstract;
+
+namespace Intentions.Pl.Menus;
+
+internal sealed class MainMenu : DialogMenu<string>
+{
+    protected override string Ask()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void ProcessAnswer(string answer)
+    {
+        throw new NotImplementedException();
+    }
+}
